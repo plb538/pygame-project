336ccbd036657eb8d17edccbbefce7d332e0f2c3
Patrick Bonnell <patrick.bonnell@verafin.com>
2023-10-02 16:49:19 -0230

    Cleanup

--- main.py
+++ main.py
@@ -3,17 +3,15 @@
 from src.game.game import Game
 
 import pygame as pg
-import traceback
 
 
 if __name__ == "__main__":
 	try:
 		pg.init()
-		g = Game()
-		g.start()
-	except Exception:
-		traceback.print_exc()
-		raise
+		game = Game()
+		game.start()
+	except Exception as ex:
+		raise ex
 	finally:
 		pg.quit()
 		quit()
